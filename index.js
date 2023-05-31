a1b5d35d5299f89f5dcc711040d81fc2a41caccf
Draian123 <beiteldennis3@yahoo.de>
2023-05-31 02:42:37 +0200

    first push

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+const { registerUser, loginUser } = require('./controllers/auth');
+const { dashBoardData } = require('./controllers/dashboard');
+
+require('dotenv').config();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/linkTree-9').then(()=>{console.log(`mongodb COnnected`)}).catch(err=>{console.log(err.message)});
+
+app.get('/', (req, res)=>{
+    res.send(`Server is running on port ${port}`);
+})
+
+app.post('/api/register', registerUser);
+app.post('/api/login', loginUser);
+app.post('/data/dashboard', dashBoardData);
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
+})
