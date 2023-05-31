c158951532f9eb61076317d370ab452bc1836262
Draian123 <beiteldennis3@yahoo.de>
2023-06-01 01:17:49 +0200

    alot done

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 const { registerUser, loginUser } = require('./controllers/auth');
 const { dashBoardData } = require('./controllers/dashboard');
+const { getUserData } = require('./controllers/getUserData');
+const { loadLinks, loadSocials } = require('./controllers/loadPrevious');
+const { saveSocials, saveProfile, saveLinks } = require('./controllers/saveItems');
+
+
 
 require('dotenv').config();
 
@@ -19,9 +24,18 @@ app.get('/', (req, res)=>{
     res.send(`Server is running on port ${port}`);
 })
 
+app.get('/get/:handle', getUserData);
+
 app.post('/api/register', registerUser);
 app.post('/api/login', loginUser);
 app.post('/data/dashboard', dashBoardData);
+// Added logic for loading saved data, and add new data
+
+app.post('/save/socials', saveSocials)
+app.post('/save/profile', saveProfile)
+app.post('/save/links', saveLinks)
+app.post('/load/socials', loadSocials)
+app.post('/load/links', loadLinks)
 
 const port = process.env.PORT || 8080;
 
